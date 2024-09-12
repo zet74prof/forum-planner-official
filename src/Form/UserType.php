8e4fd9900213be1fd487f83044e3d43991834dce
OmegazonCMoi <fabroc300@gmail.com>
2024-09-12 14:56:03 +0200

    OUAIS LA CITÉE

---
+++ src/Form/UserType.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'label' => 'Username',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Enter your username'
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Enter your email'
+                ],
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Password',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Enter your password'
+                ],
+            ])
+            ->add('enable_2fa', CheckboxType::class, [
+                'label' => "Activer l'authentification à deux facteurs",
+                'required' => false,
+                'attr' => [
+                    'class' => 'w-6 h-6 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500',
+                ],
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Register',
+                'attr' => ['class' => 'w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500']
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
