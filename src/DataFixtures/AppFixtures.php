6d48cbfea494a6f5f776d8f9eeaab6a1d64574fb
Bili-and-sheep <70603677+Bili-and-sheep@users.noreply.github.com>
2024-11-19 11:57:55 +0100

    🪇💅🏻

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use App\Entity\User;
+use function Sodium\add;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+        // create 20 user! Bam!
+        for ($i = 0; $i < 20; $i++) {
+            $user = new User();
+            $user->setNom($faker->lastName);
+            $user->setPrenom($faker->firstName);
+            $user->setUsername($faker->userName);
+            $user->setEmail($faker->email);
+            $user->setPassword($faker->password);
+            $user->setRoles($faker->randomElement([['ROLE_USER'], ['ROLE_ADMIN']]));
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
